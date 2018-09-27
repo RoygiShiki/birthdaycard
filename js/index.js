4a25f06e327a3849b69ddb8fc20d0e9cd0e142e1
mixmedia <chenshi2229771@hotmail.com>
2018-09-27 17:21:35 +0800

    delete all waste

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 $(function(){
     $('.card').click(function(e){
-        // $('ul li').removeClass('active');
         event.stopPropagation();
         $('.card').addClass('active');
-        console.log('yeah111');
     })
 
     $('html').click(function(){
-        // $(this).addClass('active');
-        console.log('yeah222');
         $('.card').removeClass('active');
     })
 })
